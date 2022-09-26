65b2b73e06d7db4e9a7e1327f0de517793e90c88
filipesenaifirjan <filipemattossenai@gmail.com>
2022-09-26 13:38:05 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿ class Program
+{
+    static void Main(string[] args)
+    {
+        
+        int valor1;
+        int valor2;
+
+        Console.WriteLine("digite o primeiro valor");
+        valor1= int.Parse(Console.ReadLine());
+        Console.WriteLine("O primeiro valor é: " + valor1);
+Console.WriteLine("digite o segundo valor");
+        valor2= int.Parse(Console.ReadLine());
+        Console.WriteLine("O primeiro valor é: " + valor2);
+    }
+}

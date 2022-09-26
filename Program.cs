b7c3c5b20769fbfb06e0fc0a24ea08366f21f29b
filipesenaifirjan <filipemattossenai@gmail.com>
2022-09-26 13:52:24 -0300

    leitura e soma dos valores

--- Program.cs
+++ Program.cs
@@ -5,12 +5,17 @@
         
         int valor1;
         int valor2;
+        int total;
 
         Console.WriteLine("digite o primeiro valor");
         valor1= int.Parse(Console.ReadLine());
         Console.WriteLine("O primeiro valor é: " + valor1);
-Console.WriteLine("digite o segundo valor");
+
+        Console.WriteLine("digite o segundo valor");
         valor2= int.Parse(Console.ReadLine());
-        Console.WriteLine("O primeiro valor é: " + valor2);
+        Console.WriteLine("O segundo valor é: " + valor2);
+
+        total = valor1 + valor2;
+        Console.WriteLine("A soma dos dois valores é: " + total);
     }
 }
